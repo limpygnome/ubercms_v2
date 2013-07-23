f23f41a422f6114bd594b4c3dad2ea398d1f827d
limpygnome <limpygnome@gmail.com>
2013-07-23 22:31:47 +0100

    Added versioning class.

---
+++ Code/App_Code/CMS/Base/Version.cs
@@ -0,0 +1,63 @@
+﻿/*                       ____               ____________
+ *                      |    |             |            |
+ *                      |    |             |    ________|
+ *                      |    |             |   |
+ *                      |    |             |   |    
+ *                      |    |             |   |    ____
+ *                      |    |             |   |   |    |
+ *                      |    |_______      |   |___|    |
+ *                      |            |  _  |            |
+ *                      |____________| |_| |____________|
+ *                        
+ *      Author(s):      limpygnome (Marcus Craske)              limpygnome@gmail.com
+ * 
+ *      License:        Creative Commons Attribution-ShareAlike 3.0 Unported
+ *                      http://creativecommons.org/licenses/by-sa/3.0/
+ * 
+ *      Path:           /CMS/Base/Version.cs
+ * 
+ *      Change-Log:
+ *                      2013-07-23      Created initial class.
+ *                      
+ * *********************************************************************************************************************
+ * Version information about the CMS.
+ * *********************************************************************************************************************
+ */
+using System;
+
+namespace CMS.Base
+{
+    public static class Version
+    {
+        /// <summary>
+        /// The current version major.
+        /// </summary>
+        public int Major
+        {
+            get
+            {
+                return 2;
+            }
+        }
+        /// <summary>
+        /// The current version minor.
+        /// </summary>
+        public int Minor
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        /// <summary>
+        /// The current version build.
+        /// </summary>
+        public int Build
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
